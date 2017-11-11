3fefef50977d866d167fdb4f650a0a3962ea41c2
Jouni Kantola <jouni@kantola.se>
2017-11-11 07:07:09 +0100

    Assets outputted as cshtml

--- test/plugin-test.js
+++
@@ -1,7 +0,0 @@
-import test from "ava";
-import RazorPartialViewsWebpackPlugin from "../lib/plugin";
-
-test("defaults to emtpy ruleset", t => {
-  new RazorPartialViewsWebpackPlugin();
-  t.pass();
-});
