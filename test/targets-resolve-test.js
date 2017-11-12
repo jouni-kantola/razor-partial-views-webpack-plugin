f69ec4e80a19a69da611eaf86001c425dac2d129
Jouni Kantola <jouni@kantola.se>
2017-11-13 00:39:16 +0100

    Invalid target fallback to C#

--- test/targets-resolve-test.js
+++ test/targets-resolve-test.js
@@ -9,6 +9,13 @@ test("default to target C#", t => {
   t.is(target.extension, "cshtml");
 });
 
+test("non-match fallback to C#", t => {
+  const target = targets.resolve("no-match");
+
+  t.is(target.lang, "C#");
+  t.is(target.extension, "cshtml");
+});
+
 test("can set target in anycase", t => {
   const target = targets.resolve("vB");
 
