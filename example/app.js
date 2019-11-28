23da1db794fe35a7f9bb1e41f309089a3140ed49
Jouni Kantola <jouni@kantola.se>
2019-11-28 02:08:36 +0100

    Unnecessarily defined variable

--- example/app.js
+++ example/app.js
@@ -1,5 +1,5 @@
 import { foo } from "./deps/module-tree-shaking.js";
-import appStyles from "./app.css";
+import "./app.css";
 
 async function bootstrap() {
   const { a } = await import("./deps/module-a.js");
--- example/deps/module-a.js
+++ example/deps/module-a.js
@@ -1,6 +1,6 @@
 import is from "is-thirteen";
 
-import moduleStyles from "./module-a.css";
+import "./module-a.css";
 
 export const a = () =>
   console.log("module a says 12 + 1 === 13", is(12 + 1).thirteen());
