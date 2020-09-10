a7f851f017e960540c9e0bbb6517067d7159c684
Jouni Kantola <jouni@kantola.se>
2020-09-10 21:14:22 +0200

    Updated base plugin

--- test/plugin-apply-test.js
+++ test/plugin-apply-test.js
@@ -2,7 +2,15 @@ import path from "path";
 import test from "ava";
 import webpack from "webpack";
 import { EOL } from "os";
+import rimraf from "rimraf";
+import fs from "fs";
 import RazorPartialViewsWebpackPlugin from "../lib/plugin";
+
+const OUTPUT_PATH = path.join(__dirname, "dist");
+
+test.beforeEach.cb(t => {
+  rimraf(OUTPUT_PATH, t.end);
+});
 
 test.cb("emit cshtml asset", t => {
   const plugin = new RazorPartialViewsWebpackPlugin({
@@ -19,7 +27,7 @@ test.cb("emit cshtml asset", t => {
         asset: path.join(__dirname, "plugin-apply-test-entry.js")
       },
       output: {
-        path: path.join(__dirname, "dist")
+        path: OUTPUT_PATH
       },
       plugins: [plugin]
     },
@@ -32,11 +40,19 @@ test.cb("emit cshtml asset", t => {
 
       const { compilation } = stats;
 
-      const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}"></script>${EOL}`;
+      const [chunk] = compilation.chunks;
+      const [file] = chunk.files;
 
+      const expected = `<script type="text/javascript" src="/${file}"></script>${EOL}`;
       const asset = compilation.assets["asset.cshtml"];
-      t.is(asset.source(), expected);
       t.is(asset.size(), expected.length);
+
+      const source = fs.readFileSync(
+        path.join(OUTPUT_PATH, "asset.cshtml"),
+        "utf8"
+      );
+
+      t.is(source, expected);
 
       t.end();
     }
