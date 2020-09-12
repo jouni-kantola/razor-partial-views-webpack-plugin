82afd9f12f1250a29972d13507ded4b5bb9af512
Jouni Kantola <jouni@kantola.se>
2020-09-12 10:56:15 +0200

    Default output assets for JS and CSS

--- test/plugin-apply-test.js
+++ test/plugin-apply-test.js
@@ -42,3 +42,36 @@ test.cb("emit cshtml asset", t => {
     }
   );
 });
+
+test.cb("default generate templated ", t => {
+  const plugin = new RazorPartialViewsWebpackPlugin();
+
+  webpack(
+    {
+      entry: {
+        asset: path.join(__dirname, "plugin-apply-test-entry.js")
+      },
+      output: {
+        path: path.join(__dirname, "dist")
+      },
+      plugins: [plugin]
+    },
+    (err, stats) => {
+      if (err) {
+        return t.end(err);
+      } else if (stats.hasErrors()) {
+        return t.end(stats.toString());
+      }
+
+      const { compilation } = stats;
+
+      const expected = `<script type="text/javascript" src="/${compilation.chunks[0].files[0]}"></script>${EOL}`;
+
+      const asset = compilation.assets["asset.cshtml"];
+      t.is(asset.source(), expected);
+      t.is(asset.size(), expected.length);
+
+      t.end();
+    }
+  );
+});
