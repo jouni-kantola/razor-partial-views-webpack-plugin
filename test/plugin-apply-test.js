452d207db75bf0e23c7b818cf03d66f05c6f585d
Jouni Kantola <jouni@kantola.se>
2020-11-05 01:18:29 +0100

    Ensure overriden output location generates expected content

--- test/plugin-apply-test.js
+++ test/plugin-apply-test.js
@@ -101,3 +101,60 @@ test.serial.cb("default generate templated assets", t => {
     }
   );
 });
+
+test.serial.cb("override default output location", t => {
+  const outputLocation = path.join(OUTPUT_PATH, "override");
+  const plugin = new RazorPartialViewsWebpackPlugin({
+    rules: [
+      {
+        name: "asset",
+        output: {
+          path: outputLocation,
+          emitAsset: false
+        }
+      }
+    ]
+  });
+
+  const webpackVersion = !!webpack.version && +webpack.version[0];
+  webpack(
+    {
+      entry: {
+        asset: path.join(__dirname, "plugin-apply-test-entry.js")
+      },
+      output: {
+        path: path.join(__dirname, "dist"),
+        filename:
+          webpackVersion >= 4
+            ? "[name].[contenthash].js"
+            : "[name].[chunkhash].js"
+      },
+      plugins: [plugin]
+    },
+    (err, stats) => {
+      if (err) {
+        return t.end(err);
+      } else if (stats.hasErrors()) {
+        return t.end(stats.toString());
+      }
+
+      const { compilation } = stats;
+
+      const [chunk] = compilation.chunks;
+      const [file] = chunk.files;
+
+      const expected = `<script type="text/javascript" src="${file}"></script>${EOL}`;
+      const asset = compilation.assets["asset.cshtml"];
+      t.is(asset, undefined);
+
+      const source = fs.readFileSync(
+        path.join(outputLocation, "asset.cshtml"),
+        "utf8"
+      );
+
+      t.is(source, expected);
+
+      t.end();
+    }
+  );
+});
